388d24b2795ce6d5698daaa2d0b3fb67650bb737
Otavio Miranda <hotavio10@gmail.com>
2024-02-02 15:07:54 -0300

    feat: update

--- app/(home)/_components/barbershop-items.tsx
+++ app/(home)/_components/barbershop-items.tsx
@@ -1,24 +1,36 @@
+"use client";
+
+
 import { Badge } from "@/app/_components/ui/badge";
 import { Button } from "@/app/_components/ui/button";
 import { Card, CardContent } from "@/app/_components/ui/card";
 import { Barbershop } from "@prisma/client";
 import { StarIcon } from "lucide-react";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 interface BarbershopItemsProps {
   barbershop: Barbershop;
 }
 
 const BarbershopItem = ({ barbershop }: BarbershopItemsProps) => {
+  const router = useRouter();
+
+
+  const handleBookingClick = () => {
+    router.push(`/barbershops/${barbershop.id}`);
+  };
 
   return (
     <Card className="min-w-[167px] max-w-[167px] rounded-2xl">
       <CardContent className="px-1 py-0">
         <div className="w-full h-[159px] relative">
+          <div className="absolute top-2 left-2 z-50">
           <Badge variant="secondary" className="opacity-90 flex gap-1 items-center absolute top-2 left-2 z-50">
           <StarIcon size={12} className="fill-primary text-primary"/>
           <span className="text-xs">5,0</span>
           </Badge>
+          </div>
           <Image
             alt={barbershop.name}
             src={barbershop.imageUrl}
@@ -32,7 +44,7 @@ const BarbershopItem = ({ barbershop }: BarbershopItemsProps) => {
         <div className="px-2 pb-3">
           <h2 className="font-bold mt-2 overflow-hidden text-ellipsis text-nowrap">{barbershop.name}</h2>
           <p className="text-sm text-gray-400 overflow-hidden text-ellipsis text-nowrap">{barbershop.address}</p>
-          <Button className="w-full mt-2" variant="secondary">
+          <Button className="w-full mt-3" variant="secondary" onClick={handleBookingClick}>
             Reservar
           </Button>
         </div>
@@ -40,5 +52,4 @@ const BarbershopItem = ({ barbershop }: BarbershopItemsProps) => {
     </Card>
   );
 };
-
 export default BarbershopItem;
