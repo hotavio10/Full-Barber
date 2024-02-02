289ea55b426fb7c09a431c0174bd39ea983b80b8
Otavio Miranda <hotavio10@gmail.com>
2024-02-02 17:18:03 -0300

    feat: add ServiceItem component

--- app/barbershops/[id]/page.tsx
+++ app/barbershops/[id]/page.tsx
@@ -1,8 +1,9 @@
 import { db } from "@/app/_lib/prisma";
 import { useRouter } from "next/router";
 import { RouterContext } from "next/dist/shared/lib/router-context.shared-runtime";
-import ServiceItem from "./[id]/_components/service-item";
-import BarbershopInfo from "./[id]/_components/barbershop-info";
+import BarbershopInfo from "./_components/barbershop-info";
+import ServiceItem from "./_components/service-item";
+
 
 
 interface BarbershopDetailsPageProps {
@@ -31,7 +32,7 @@ const BarbershopDetailsPage = async ({ params }: BarbershopDetailsPageProps) => 
   }
 
   return (
-    <div>
+    <div className="px-5 flex flex-col gap-4 py-6">
       <BarbershopInfo barbershop={barbershop} />
 
       {barbershop.services.map((service) => (
