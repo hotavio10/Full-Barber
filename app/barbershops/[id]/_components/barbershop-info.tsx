6eb9bcbe11d52d043f18ac4e39d7d5d4bdf93580
Otavio Miranda <hotavio10@gmail.com>
2024-02-05 21:17:33 -0300

    feat: add sideMenu component

--- app/barbershops/[id]/_components/barbershop-info.tsx
+++ app/barbershops/[id]/_components/barbershop-info.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-{/*import SideMenu from "@/app/_components/side-menu";*/}
+import SideMenu from "@/app/_components/side-menu";
 import { Button } from "@/app/_components/ui/button";
 import { Sheet, SheetContent, SheetTrigger } from "@/app/_components/ui/sheet";
 import { Barbershop } from "@prisma/client";
@@ -26,17 +26,18 @@ const BarbershopInfo = ({ barbershop }: BarbershopInfoProps) => {
           <ChevronLeftIcon />
         </Button>
 
-        <Sheet>
+            <Sheet>
           <SheetTrigger asChild>
             <Button size="icon" variant="outline" className="z-50 absolute top-4 right-4">
               <MenuIcon />
             </Button>
           </SheetTrigger>
 
-          {/*<SheetContent className="p-0">
-            <SideMenu />
-          </SheetContent>*/}
+          <SheetContent className="p-0">
+          <SideMenu  />
+          </SheetContent>
         </Sheet>
+          
 
         <Image
           src={barbershop.imageUrl}
